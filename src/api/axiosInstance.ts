dd1270c6f00b6330c7de9d147b2862ab9b09435b
Gagos <gago.simonyan1.y@gmail.com>
2025-02-16 22:06:25 +0400

    fix socket

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://fruitqwest.onrender.com',
+  baseURL: 'https://fruitqwest.vercel.app',
   // baseURL: 'http://localhost:3000',
   headers: {
     'Content-Type': 'application/json',
