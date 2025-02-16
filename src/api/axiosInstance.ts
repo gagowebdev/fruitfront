9b5c9d501fd76f266e49f8818ced56b813881fe8
Gagos <gago.simonyan1.y@gmail.com>
2025-02-16 21:17:22 +0400

    Обновил настройки CORS и исправил баги

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://fruitqwest.onrender.com',
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://fruitqwest.onrender.com',
+  // baseURL: 'http://localhost:3000',
   headers: {
     'Content-Type': 'application/json',
   },
